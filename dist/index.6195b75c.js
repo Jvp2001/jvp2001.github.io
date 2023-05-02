3a9ca7d97f3a19df35aeaf5cb15239b1e4ba021e
joshuapetersen <s4006219@glos.ac.uk>
2023-05-02 13:14:54 +0100

    Initial Commit.

---
+++ dist/index.6195b75c.js
@@ -0,0 +1,8 @@
+const button = document.createElement("button");
+button.innerText = "Click me";
+button.onclick = ()=>{
+    alert("Hello, World!");
+};
+document.body.appendChild(button);
+
+//# sourceMappingURL=index.6195b75c.js.map
