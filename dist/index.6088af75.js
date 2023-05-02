a9a52717610d2eaac42a692a81ffb9125c3cff24
joshuapetersen <s4006219@glos.ac.uk>
2023-05-02 14:26:56 +0100

    Updated dist.

--- dist/index.6088af75.js
+++
@@ -1,2 +0,0 @@
-const button=document.createElement("button");button.innerText="Click me",button.onclick=()=>{alert("Hello, World!")},document.body.appendChild(button);
-//# sourceMappingURL=index.6088af75.js.map
